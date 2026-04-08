f94aa6ba0f33adbe22cb88ecc8c8dd9c341fc283
mkivanc27-boop <mkivanc27@gmail.com>
2026-04-08 06:49:39 +0300

    Update MixinFastMathAdvanced.java

--- src/main/java/com/optibest/mixin/MixinFastMathAdvanced.java
+++ src/main/java/com/optibest/mixin/MixinFastMathAdvanced.java
@@ -5,17 +5,30 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Overwrite;
 
+/**
+ * @author mbest700
+ */
 @Mixin(MathHelper.class)
 public class MixinFastMathAdvanced {
 
     @Shadow
-    private static float[] SINE_TABLE; // Gizli tabloya erişim sağladık
+    private static float[] SINE_TABLE;
 
     /**
-     * @author MustafaKivanc
+     * @author mbest700
+     * @reason Ultra fast math tables for mbest700 edition
      */
     @Overwrite
     public static float sin(float value) {
         return SINE_TABLE[(int)(value * 10430.378F) & 65535];
+    }
+
+    /**
+     * @author mbest700
+     * @reason Fast cosine calculation
+     */
+    @Overwrite
+    public static float cos(float value) {
+        return SINE_TABLE[(int)(value * 10430.378F + 16384.0F) & 65535];
     }
 }
