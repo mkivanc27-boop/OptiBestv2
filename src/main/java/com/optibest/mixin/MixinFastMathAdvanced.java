9336192f4f71e0b7f7973efc75121185a7035b98
mkivanc27-boop <mkivanc27@gmail.com>
2026-04-08 06:43:13 +0300

    Update MixinFastMathAdvanced.java

--- src/main/java/com/optibest/mixin/MixinFastMathAdvanced.java
+++ src/main/java/com/optibest/mixin/MixinFastMathAdvanced.java
@@ -2,14 +2,20 @@ package com.optibest.mixin;
 
 import net.minecraft.util.math.MathHelper;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Overwrite;
 
 @Mixin(MathHelper.class)
 public class MixinFastMathAdvanced {
+
+    @Shadow
+    private static float[] SINE_TABLE; // Gizli tabloya erişim sağladık
+
+    /**
+     * @author MustafaKivanc
+     */
     @Overwrite
     public static float sin(float value) {
-        // Standart Java Sinüs yerine Sodium tipi hızlı tablo okuması
-        return MathHelper.SINE_TABLE[(int)(value * 10430.378F) & 65535];
+        return SINE_TABLE[(int)(value * 10430.378F) & 65535];
     }
 }
-
