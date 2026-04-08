e4bd929ead842f6d6dadbb9a5edadab6bcabd138
mkivanc27-boop <mkivanc27@gmail.com>
2026-04-08 04:06:07 +0300

    Update OptiBest.java

--- src/main/java/com/optibest/OptiBest.java
+++ src/main/java/com/optibest/OptiBest.java
@@ -1,11 +1,9 @@
 package com.optibest;
-
 import net.fabricmc.api.ClientModInitializer;
 
 public class OptiBest implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
-        System.out.println("OptiBest by mbest700 yuklendi! 1.21.4 FPS tavan.");
+        System.out.println("mbest700 OptiBest v1.0 Yuklendi!");
     }
 }
-
