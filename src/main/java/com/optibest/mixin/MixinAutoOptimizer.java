d6d9e338a9c0a8e7c609aea04c0be7cbf93bc70b
mkivanc27-boop <mkivanc27@gmail.com>
2026-04-08 05:37:48 +0300

    Update MixinAutoOptimizer.java

--- src/main/java/com/optibest/mixin/MixinAutoOptimizer.java
+++ src/main/java/com/optibest/mixin/MixinAutoOptimizer.java
@@ -2,7 +2,6 @@ package com.optibest.mixin;
 
 import net.minecraft.client.option.GameOptions;
 import net.minecraft.client.option.CloudRenderMode;
-import net.minecraft.client.option.ParticleStatus;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -14,11 +13,14 @@ public class MixinAutoOptimizer {
     private void forceBestSettings(CallbackInfo ci) {
         GameOptions options = (GameOptions) (Object) this;
 
+        // Import gerektirmeyen temel ayarlar
         options.getCloudRenderMode().setValue(CloudRenderMode.OFF);
-        options.getParticles().setValue(ParticleStatus.MINIMAL);
         options.getAo().setValue(false);
         options.getViewDistance().setValue(2);
         options.getEntityDistanceScaling().setValue(0.5);
         options.getMipmapLevels().setValue(0);
+        
+        // Parçacık hatasını önlemek için bu satırı geçici olarak sildim, 
+        // Build almanı engellemesin.
     }
 }
