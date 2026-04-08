b843d34a9a480d6e6e8b60ef7fc4ddb9d72f1a9b
mkivanc27-boop <mkivanc27@gmail.com>
2026-04-08 05:06:54 +0300

    Create MixinFontRenderer.java

---
+++ src/main/java/com/optibest/mixin/MixinFontRenderer.java
@@ -0,0 +1,15 @@
+package com.optibest.mixin;
+
+import net.minecraft.client.font.TextRenderer;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+
+@Mixin(TextRenderer.class)
+public class MixinTextRenderer {
+    @ModifyVariable(method = "drawInternal", at = @At("HEAD"), ordinal = 0, argsOnly = true)
+    private boolean disableShadow(boolean shadow) {
+        return false;
+    }
+}
+
