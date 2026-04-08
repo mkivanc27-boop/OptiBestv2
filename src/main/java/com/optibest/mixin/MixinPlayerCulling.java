43c4949895305c0579bd66113c9ed9ef059c7ea4
mkivanc27-boop <mkivanc27@gmail.com>
2026-04-08 06:50:06 +0300

    Update MixinPlayerCulling.java

--- src/main/java/com/optibest/mixin/MixinPlayerCulling.java
+++ src/main/java/com/optibest/mixin/MixinPlayerCulling.java
@@ -2,18 +2,22 @@ package com.optibest.mixin;
 
 import net.minecraft.client.network.OtherClientPlayerEntity;
 import net.minecraft.client.render.entity.PlayerEntityRenderer;
+import net.minecraft.client.render.Frustum;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+/**
+ * @author mbest700
+ */
 @Mixin(PlayerEntityRenderer.class)
 public class MixinPlayerCulling {
-    @Inject(method = "shouldRender(Lnet/minecraft/client/network/OtherClientPlayerEntity;Lnet/minecraft/client/render/Frustum;DDD)Z", at = @At("HEAD"), cancellable = true)
-    private void cullPlayers(OtherClientPlayerEntity player, net.minecraft.client.render.Frustum frustum, double x, double y, double z, CallbackInfoReturnable<Boolean> cir) {
-        if (player.isInvisible() || player.isSpectator()) {
+    
+    @Inject(method = "shouldRender", at = @At("HEAD"), cancellable = true)
+    private void mbest700$cullPlayers(OtherClientPlayerEntity entity, Frustum frustum, double x, double y, double z, CallbackInfoReturnable<Boolean> cir) {
+        if (entity.isInvisible()) {
             cir.setReturnValue(false);
         }
     }
 }
-
