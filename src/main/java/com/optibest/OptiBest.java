eaee07a4f016cf668ebc6c4973c33d5daf695112
mkivanc27-boop <mkivanc27@gmail.com>
2026-04-08 07:05:22 +0300

    Update OptiBest.java

--- src/main/java/com/optibest/OptiBest.java
+++ src/main/java/com/optibest/OptiBest.java
@@ -1,9 +1,19 @@
 package com.optibest;
-import net.fabricmc.api.ClientModInitializer;
 
-public class OptiBest implements ClientModInitializer {
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author mbest700
+ */
+public class OptiBest implements ModInitializer {
+    public static final String MOD_ID = "optibest";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
     @Override
-    public void onInitializeClient() {
-        System.out.println("mbest700 OptiBest v1.0 Yuklendi!");
+    public void onInitialize() {
+        // Bu kısım oyun açılırken çalışır
+        LOGGER.info("OptiBest: mbest700 Edition başarıyla yüklendi! FPS uçuşa hazır.");
     }
 }
