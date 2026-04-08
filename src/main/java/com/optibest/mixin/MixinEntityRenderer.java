07ee5a0de2e8dc8a2aa923ad070c42c2b47d30ca
mkivanc27-boop <mkivanc27@gmail.com>
2026-04-08 04:17:16 +0300

    Update MixinEntityRenderer.java

--- src/main/java/com/optibest/mixin/MixinEntityRenderer.java
+++ src/main/java/com/optibest/mixin/MixinEntityRenderer.java
@@ -1,19 +1,31 @@
 package com.optibest.mixin;
 
-import net.minecraft.block.Block;
-import net.minecraft.block.BlockState;
-import net.minecraft.util.math.Direction;
+import com.optibest.config.OptiBestConfig;
+import net.minecraft.client.render.Frustum;
+import net.minecraft.client.render.entity.EntityRenderer;
+import net.minecraft.entity.Entity;
+import net.minecraft.client.MinecraftClient;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(Block.class)
-public class MixinBlockOcclusion {
-    @Inject(method = "shouldSideBeRendered", at = @At("HEAD"), cancellable = true)
-    private static void onShouldSideBeRendered(BlockState state, BlockState adjacentState, Direction side, CallbackInfoReturnable<Boolean> cir) {
-        if (state == adjacentState) {
+@Mixin(EntityRenderer.class)
+public class MixinEntityRenderer<T extends Entity> {
+    @Inject(method = "shouldRender", at = @At("HEAD"), cancellable = true)
+    private void onShouldRender(T entity, Frustum frustum, double x, double y, double z, CallbackInfoReturnable<Boolean> cir) {
+        // Görüş açısı dışında kalanları çizmez
+        if (!frustum.isVisible(entity.getBoundingBox())) {
             cir.setReturnValue(false);
+            return;
+        }
+
+        // mbest700 Özel: 25 blok ötesi kısıtlaması
+        if (OptiBestConfig.aggressiveCulling) {
+            double distSq = entity.squaredDistanceTo(MinecraftClient.getInstance().player);
+            if (distSq > 625 && !entity.isPlayer()) {
+                cir.setReturnValue(false);
+            }
         }
     }
 }
