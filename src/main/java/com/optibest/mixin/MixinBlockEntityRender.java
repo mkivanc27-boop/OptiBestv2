c48c828932d27b97f2293ac1549de984c623eafb
mkivanc27-boop <mkivanc27@gmail.com>
2026-04-08 05:28:15 +0300

    Update MixinBlockEntityRender.java

--- src/main/java/com/optibest/mixin/MixinBlockEntityRender.java
+++ src/main/java/com/optibest/mixin/MixinBlockEntityRender.java
@@ -1,5 +1,6 @@
 package com.optibest.mixin;
 
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.render.block.entity.BlockEntityRenderDispatcher;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.client.util.math.MatrixStack;
@@ -13,10 +14,9 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class MixinBlockEntityRender {
     @Inject(method = "render", at = @At("HEAD"), cancellable = true)
     private <E extends BlockEntity> void onRender(E blockEntity, float tickDelta, MatrixStack matrices, VertexConsumerProvider vertexConsumers, CallbackInfo ci) {
-        // Uzaktaki veya görünmeyen sandık/fırın renderını iptal eder
-        if (blockEntity.hasWorld() && blockEntity.getPos().getSquaredDistance(MinecraftClient.getInstance().player.getPos()) > 1024) {
+        MinecraftClient client = MinecraftClient.getInstance();
+        if (client.player != null && blockEntity.hasWorld() && blockEntity.getPos().toCenterPos().squaredDistanceTo(client.player.getPos()) > 1024) {
             ci.cancel(); 
         }
     }
 }
-
