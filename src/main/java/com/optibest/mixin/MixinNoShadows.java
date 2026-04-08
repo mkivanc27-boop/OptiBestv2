6291e0c1b18694093fc32109fe933079dd243e8b
mkivanc27-boop <mkivanc27@gmail.com>
2026-04-08 04:37:42 +0300

    Update MixinNoShadows.java

--- src/main/java/com/optibest/mixin/MixinNoShadows.java
+++ src/main/java/com/optibest/mixin/MixinNoShadows.java
@@ -1,16 +1,15 @@
 package com.optibest.mixin;
 
-import net.minecraft.client.render.entity.EntityRenderDispatcher;
+import net.minecraft.client.render.entity.EntityRenderer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(EntityRenderDispatcher.class)
+@Mixin(EntityRenderer.class)
 public class MixinNoShadows {
-    @Inject(method = "renderShadow", at = @At("HEAD"), cancellable = true)
-    private static void cancelShadows(CallbackInfo ci) {
-        ci.cancel(); 
+    @Inject(method = "getShadowRadius", at = @At("HEAD"), cancellable = true)
+    private void onGetShadowRadius(CallbackInfoReturnable<Float> cir) {
+        cir.setReturnValue(0.0f); // Tüm gölge yarıçaplarını 0 yapar
     }
 }
-
