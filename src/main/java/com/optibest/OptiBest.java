0571738a4f9875d73b2ac21e56f17d59bb425553
mkivanc27-boop <mkivanc27@gmail.com>
2026-04-08 03:59:48 +0300

    Create OptiBest.java

---
+++ src/main/java/com/optibest/OptiBest.java
@@ -0,0 +1,11 @@
+package com.optibest;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class OptiBest implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        System.out.println("OptiBest by mbest700 yuklendi! 1.21.4 FPS tavan.");
+    }
+}
+
