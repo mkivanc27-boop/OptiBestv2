cb57cb1b3ebe276dc6b7b4506f82c428b2dd170a
mkivanc27-boop <mkivanc27@gmail.com>
2026-04-08 04:52:38 +0300

    Update MixinParticleManager.java

--- src/main/java/com/optibest/mixin/MixinParticleManager.java
+++ src/main/java/com/optibest/mixin/MixinParticleManager.java
@@ -2,17 +2,16 @@ package com.optibest.mixin;
 
 import net.minecraft.client.particle.Particle;
 import net.minecraft.client.particle.ParticleManager;
-import net.minecraft.client.particle.ParticleTextureSheet;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ParticleManager.class)
 public class MixinParticleManager {
-    // 1.21.4 sürümünde addParticle artık bir değer döndürdüğü için Returnable kullanmalıyız
+    // Returnable yerine düz CallbackInfo kullanarak çakışmayı önlüyoruz
     @Inject(method = "addParticle(Lnet/minecraft/client/particle/Particle;)V", at = @At("HEAD"), cancellable = true)
-    private void onAdd(Particle particle, CallbackInfoReturnable<Particle> cir) {
-        // Gereksiz parçacıkları limitlemek için buraya ileride mantık ekleyebilirsin
+    private void onAdd(Particle particle, CallbackInfo ci) {
+        // Metod void olduğu için düz CallbackInfo yeterli
     }
 }
