4ef63fa27123e21315b461b4609e1fc4a5b55a9b
mkivanc27-boop <mkivanc27@gmail.com>
2026-04-09 17:51:15 +0300

    Update MixinExtremeFog.java

--- src/main/java/com/optibest/mixin/MixinExtremeFog.java
+++ src/main/java/com/optibest/mixin/MixinExtremeFog.java
@@ -1,26 +1,20 @@
 package com.optibest.mixin;
 
-import net.minecraft.client.render.BackgroundRenderer;
-import net.minecraft.client.render.Camera;
 import com.optibest.OptiBestConfig;
 import com.mojang.blaze3d.systems.RenderSystem;
-import net.minecraft.util.math.MathHelper;
+import net.minecraft.client.render.BackgroundRenderer;
+import net.minecraft.client.render.Camera;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(BackgroundRenderer.class)
-public class MixinExtremeFog { // DOSYA ADIYLA AYNI OLMAK ZORUNDA
-
+public class MixinExtremeFog {
     @Inject(method = "applyFog", at = @At("RETURN"))
-    private static void mbest700$animatedDynamicFog(Camera camera, BackgroundRenderer.FogType fogType, float viewDistance, boolean thickFog, float tickDelta, CallbackInfo ci) {
+    private static void onApplyFog(Camera camera, BackgroundRenderer.FogType fogType, float viewDistance, boolean thickFog, float tickDelta, CallbackInfo ci) {
         if (OptiBestConfig.extremeCulling) {
-            long time = System.currentTimeMillis();
-            float animation = MathHelper.sin((float) (time % 3000L) / 3000.0F * (float)Math.PI * 2.0F) * 1.5F;
-            float baseLimit = (float) OptiBestConfig.renderDistanceLimit;
-            
-            float dynamicEnd = baseLimit + animation;
+            float dynamicEnd = (float) OptiBestConfig.renderDistanceLimit;
             RenderSystem.setShaderFogStart(dynamicEnd * 0.15F);
             RenderSystem.setShaderFogEnd(dynamicEnd);
         }
