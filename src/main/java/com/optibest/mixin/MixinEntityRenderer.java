3412e7483b2bc6b4762b0fe4280a00a5dd7c0754
mkivanc27-boop <mkivanc27@gmail.com>
2026-04-09 17:47:34 +0300

    Update MixinEntityRenderer.java

--- src/main/java/com/optibest/mixin/MixinEntityRenderer.java
+++ src/main/java/com/optibest/mixin/MixinEntityRenderer.java
@@ -1,6 +1,5 @@
 package com.optibest.mixin;
 
-import com.optibest; // Sende package com.optibest ise böyle kalmalı
 import com.optibest.OptiBestConfig;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.render.Frustum;
@@ -19,24 +18,17 @@ public abstract class MixinEntityRenderer<T extends Entity> {
 
     @Inject(method = "shouldRender", at = @At("HEAD"), cancellable = true)
     private void mbest700$optimizeEntityRender(T entity, Frustum frustum, double x, double y, double z, CallbackInfoReturnable<Boolean> cir) {
-        // 1. Frustum (Görüş açısı) kontrolü zaten Minecraft'ta var ama biz garantiye alıyoruz
         if (!frustum.isVisible(entity.getBoundingBox())) {
             cir.setReturnValue(false);
             return;
         }
 
-        // 2. mbest700 Özel: Agresif Culling (25 Blok Sınırı)
         if (OptiBestConfig.aggressiveCulling) {
             var player = MinecraftClient.getInstance().player;
-            
-            // Oyuncu yoksa veya render edilen şey oyuncunun kendisiyse dokunma
             if (player == null || entity == player) return;
 
-            // 25 blok = 625 squared distance
             double distSq = entity.squaredDistanceTo(player.getX(), player.getY(), player.getZ());
-            
             if (distSq > 625.0) {
-                // Sadece dekoratif/önemsiz varlıkları gizle, oyuncuları ve bossları gizleme
                 if (!entity.isPlayer()) {
                     cir.setReturnValue(false);
                 }
