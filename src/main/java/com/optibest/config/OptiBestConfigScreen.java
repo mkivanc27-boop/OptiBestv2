b2e01eb4943fdde9db28d81ea302e6d78261f324
mkivanc27-boop <mkivanc27@gmail.com>
2026-04-09 17:50:46 +0300

    Update OptiBestConfigScreen.java

--- src/main/java/com/optibest/config/OptiBestConfigScreen.java
+++ src/main/java/com/optibest/config/OptiBestConfigScreen.java
@@ -1,35 +1,20 @@
 package com.optibest.config;
 
+import com.optibest.OptiBestConfig; // Doğru paket yolu
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.widget.ButtonWidget;
-import net.minecraft.client.gui.DrawContext;
 import net.minecraft.text.Text;
 
 public class OptiBestConfigScreen extends Screen {
-    private final Screen parent;
-
-    public OptiBestConfigScreen(Screen parent) {
-        super(Text.literal("OptiBest by mbest700"));
-        this.parent = parent;
+    protected OptiBestConfigScreen(Text title) {
+        super(title);
     }
 
     @Override
     protected void init() {
-        this.addDrawableChild(ButtonWidget.builder(Text.literal("Agresif Culling (25 Blok): " + (OptiBestConfig.aggressiveCulling ? "AÇIK" : "KAPALI")), button -> {
+        this.addDrawableChild(ButtonWidget.builder(Text.literal("Agresif Culling: " + (OptiBestConfig.aggressiveCulling ? "AÇIK" : "KAPALI")), button -> {
             OptiBestConfig.aggressiveCulling = !OptiBestConfig.aggressiveCulling;
-            button.setMessage(Text.literal("Agresif Culling (25 Blok): " + (OptiBestConfig.aggressiveCulling ? "AÇIK" : "KAPALI")));
-        }).dimensions(this.width / 2 - 100, 60, 200, 20).build());
-
-        this.addDrawableChild(ButtonWidget.builder(Text.literal("Geri Dön"), button -> {
-            this.client.setScreen(this.parent);
-        }).dimensions(this.width / 2 - 100, this.height - 40, 200, 20).build());
-    }
-
-    @Override
-    public void render(DrawContext context, int mouseX, int mouseY, float delta) {
-        this.renderBackground(context, mouseX, mouseY, delta);
-        context.drawCenteredTextWithShadow(this.textRenderer, this.title, this.width / 2, 20, 0x00FF00);
-        super.render(context, mouseX, mouseY, delta);
+            button.setMessage(Text.literal("Agresif Culling: " + (OptiBestConfig.aggressiveCulling ? "AÇIK" : "KAPALI")));
+        }).dimensions(this.width / 2 - 100, 50, 200, 20).build());
     }
 }
-
