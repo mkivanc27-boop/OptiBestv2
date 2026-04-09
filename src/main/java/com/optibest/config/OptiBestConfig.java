d57b819e17c6eef0bbf722c2f971992a535c0790
mkivanc27-boop <mkivanc27@gmail.com>
2026-04-09 17:06:34 +0300

    Update OptiBestConfig.java

--- src/main/java/com/optibest/config/OptiBestConfig.java
+++ src/main/java/com/optibest/config/OptiBestConfig.java
@@ -1,6 +1,7 @@
 package com.optibest;
 
 public class OptiBestConfig {
+    // Mod Menu'den kontrol edilecek değişkenler
     public static boolean extremeCulling = false;
-    public static int renderDistanceLimit = 10; // Varsayılan 10 blok
+    public static int renderDistanceLimit = 10; 
 }
