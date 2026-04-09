cb52ff481789f71bf3998452a67fcdb0a2fd7afe
mkivanc27-boop <mkivanc27@gmail.com>
2026-04-09 17:42:50 +0300

    Update OptiBestConfig.java

--- src/main/java/com/optibest/config/OptiBestConfig.java
+++ src/main/java/com/optibest/config/OptiBestConfig.java
@@ -1,7 +1,12 @@
 package com.optibest;
 
 public class OptiBestConfig {
+    // Sisin çalışması için gereken değişken
     public static boolean extremeCulling = false;
-    public static boolean aggressiveCulling = false; // Diğer mixinlerin hata vermemesi için şart!
+    
+    // Diğer Mixin'lerin (EntityRenderer, ConfigScreen) hata vermemesi için bu ŞART:
+    public static boolean aggressiveCulling = false; 
+    
+    // Sis mesafesi
     public static int renderDistanceLimit = 10;
 }
