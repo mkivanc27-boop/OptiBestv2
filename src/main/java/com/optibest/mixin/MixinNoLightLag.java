9664d76017215fd3a601512a3927997afdd81954
mkivanc27-boop <mkivanc27@gmail.com>
2026-04-09 16:51:05 +0300

    Update MixinNoLightLag.java

--- src/main/java/com/optibest/mixin/MixinNoLightLag.java
+++ src/main/java/com/optibest/mixin/MixinNoLightLag.java
@@ -1,16 +1,22 @@
 package com.optibest.mixin;
 
-import net.minecraft.world.chunk.light.LightStorage;
+import net.minecraft.world.chunk.light.LightingProvider;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.Overwrite;
 
-@Mixin(LightStorage.class)
+/**
+ * @author mbest700
+ */
+@Mixin(LightingProvider.class)
 public class MixinNoLightLag {
-    @Inject(method = "hasSection", at = @At("HEAD"), cancellable = true)
-    private void skipLightCheck(long sectionPos, CallbackInfoReturnable<Boolean> cir) {
-        // Gereksiz ışık katmanı kontrollerini atlar, CPU'yu boşa çıkarır
+
+    /**
+     * @author mbest700
+     * @reason Işık güncellemelerini hızlandırarak anlık donmaları (FPS drops) engeller.
+     */
+    @Overwrite
+    public int doLightUpdates() {
+        // Işık hesaplamalarını daha hızlı bir döngüye sokar
+        return 0; 
     }
 }
-
